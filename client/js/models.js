04600c717b90b64dbb31a543c291478cc7ba0937
AronVanAmmers <aron@certinio.com>
2015-08-24 21:14:05 +0200

    Remove generated JS artifacts from git

--- client/js/models.js
+++
@@ -1,2 +0,0 @@
-// The classes in this file are duplicates of the server-side model classes. The server side is the master version.
-//# sourceMappingURL=models.js.map
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-function index(req, res) {
-    // TODO: check whether we already have an authorization token for the user.
-    var fullPath = path.resolve('client/index.html');
-    res.sendFile(fullPath);
-}
-exports.index = index;
-;
-//# sourceMappingURL=index.js.map
