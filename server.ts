7d85f8e8ad9d53ac89c77691b9a7b8fcff2fc955
AronVanAmmers <aron@certinio.com>
2015-09-07 20:23:05 +0200

    Fix: load custom config file; fs.exists never worked.

--- server.ts
+++ server.ts
@@ -13,18 +13,38 @@ import oauthController = require('./controllers/oauthController');
 /*************** Configuration ********************/
 var CONFIG_FILE = './config.json';
 var config;
+var configString: string;
 
-if (!fs.exists(CONFIG_FILE))
-    CONFIG_FILE = './config.default.json';
-
+// We don't use fs.exists() to try to read the file; the recommended method is just opening and 
+// handling an error: https://nodejs.org/api/fs.html#fs_fs_exists_path_callback
 try {
-    // Strip the BOM character as readFileSync doesn't do that.
-    var configString = fs.readFileSync(CONFIG_FILE, 'utf8').replace(/^\uFEFF/, '');
+    configString = fs.readFileSync(CONFIG_FILE, 'utf8');
+}
+catch (e) {
+    try {
+        CONFIG_FILE = './config.default.json';
+        configString = fs.readFileSync(CONFIG_FILE, 'utf8');
+    }
+    catch (e2) {
+        console.log("Error while loading config file: " + e2);
+        // TODO: exit with error. No run without a valid config.
+    }
+}
+
+console.log("Using configuration from " + CONFIG_FILE);
+// Strip the BOM character as readFileSync doesn't do that.
+configString = configString.replace(/^\uFEFF/, '');
+try {
+    // Parse config file.
     config = JSON.parse(configString);
 }
 catch (e) {
     console.log("Error while parsing config file: " + e);
-}
+    // TODO: exit with error. No run without a valid config.
+ }
+
+console.log("My configuration:");
+console.log(config);
 
 var HTTP_PORT = config.server.httpPort;
 var HTTPS_PORT = config.server.httpsPort;
