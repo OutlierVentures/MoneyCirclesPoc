9f15be43635193bcd50039df30093d3179cb874f
AronVanAmmers <aron@blockstars.io>
2015-12-04 19:18:38 +0100

    Fix path issue when running on Windows introduced with Docker 1.9+

--- docker/run-blockchain.sh
+++ docker/run-blockchain.sh
@@ -3,6 +3,6 @@
 export CONTAINER_NAME=mcpoc_blockchain
 
 docker rm $CONTAINER_NAME
-docker run -p 8101:8101 -v `pwd`/..:/app \
-	 -v `pwd`/../../mcpoc_blockchain_data:/blockchain \
+docker run -p 8101:8101 -v /`pwd`/..:/app \
+	 -v /`pwd`/../../mcpoc_blockchain_data:/blockchain \
 	 --name $CONTAINER_NAME -t -i blockstars/mcpoc_blockchain bash
